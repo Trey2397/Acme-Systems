493ec2d26ea195134f648ed6e9da19d79d1cc08e
Trey2397 <romellegrn@gmail.com>
2021-08-29 03:04:36 -0500

    Added whatFor method to HelloWorld class.

--- Acme Systems/src/ver1/HelloWorld.java
+++ Acme Systems/src/ver1/HelloWorld.java
@@ -1,10 +1,21 @@
 package ver1;
 
+import java.util.Scanner;
+
 public class HelloWorld {
 	
 	public static void heyNow() {
 		String Msg = "Hey Now!";
 		System.out.println(Msg);
+	}
+	
+	public static void whatFor() {
+		Scanner stringArg = new Scanner(System.in);
+	    System.out.println("Enter a string");
+
+	    String FollowedByWhatFor = stringArg.nextLine();
+	    System.out.println(FollowedByWhatFor+ ", what for?");
+	    stringArg.close();
 	}
 
 	public static void main(String[] args) {
